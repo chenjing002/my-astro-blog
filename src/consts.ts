c7a736fe2eb46c023fd0eccc1d302cda43b5d7e7
cjcj <chenjing002@users.noreply.github.com>
2024-07-19 09:49:39 +0800

    Initialed

---
+++ src/consts.ts
@@ -0,0 +1,44 @@
+import type { Site, Metadata, Socials } from "@types";
+
+export const SITE: Site = {
+  NAME: "Chen Jing",
+  EMAIL: "hi@chenjing.tech",
+  NUM_POSTS_ON_HOMEPAGE: 4,
+  NUM_WORKS_ON_HOMEPAGE: 2,
+  NUM_PROJECTS_ON_HOMEPAGE: 3,
+};
+
+export const HOME: Metadata = {
+  TITLE: "首页",
+  DESCRIPTION: "Chen Jing's personal website.",
+};
+
+export const BLOG: Metadata = {
+  TITLE: "文章",
+  DESCRIPTION: "文字记录我生命的章节",
+};
+
+export const WORK: Metadata = {
+  TITLE: "Work",
+  DESCRIPTION: "Where I have worked and what I have done.",
+};
+
+export const PROJECTS: Metadata = {
+  TITLE: "项目",
+  DESCRIPTION: "我做过什么和正在做什么。",
+};
+
+export const SOCIALS: Socials = [
+  // { 
+  //   NAME: "hi@chenjing.tech",
+  //   HREF: "mailto:hi@chenjing.tech",
+  // } ,
+  // { 
+  //   NAME: "github",
+  //   HREF: "https://github.com/markhorn-dev"
+  // },
+  // { 
+  //   NAME: "linkedin",
+  //   HREF: "https://www.linkedin.com/in/markhorn-dev",
+  // }
+];
