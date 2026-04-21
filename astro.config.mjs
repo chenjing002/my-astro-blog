526c9bc159c02f0b437554fa5d0c26c843b5b07f
chenjing7 <1015049570@qq.com>
2026-04-20 19:48:58 -0700

    Add letters-to-shareholders project links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-nano-demo.vercel.app",
-    integrations: [tailwind(), mdx()],
+    site: "https://www.chenjing.tech",
+    integrations: [tailwind(), mdx(), sitemap()],
 });
